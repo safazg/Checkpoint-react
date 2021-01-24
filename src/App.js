79d63b6ae887934198cd2fd98385b57703db3c0e
ciao-boop <safa.zgaya@gmail.com>
2021-01-24 22:38:46 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import Form from './Components/Form/Form'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import MyNavbar from './Components/Navbar/navbar';
+
+function App() {
+  return (
+<div>
+
+  
+  <MyNavbar/>
+ <Form/>
+</div>
+   
+  );
+}
+
+export default App;
+
+
+
